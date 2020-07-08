a7bcd2c42433f08e894bd4338f93fe7063416485
hyphenc <46054695+hyphenc@users.noreply.github.com>
2020-07-08 22:44:18 +0200

    add url anchors for entries, improve css

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ cat dataset.csv | sort -o dataset.csv
 # get cutoff line number
 (( LINENUM = $(grep -no '<div id="data">' index.html | sed -n 's/:.*//p') + 1 ))
 
-REINSERT='		</div>
-	</body>
-</html>'
+REINSERT='</div></body></html>'
 
 sed -i "$LINENUM"',$d' index.html
 
@@ -18,7 +16,7 @@ cat dataset.csv | while read LINE; do
 	BACK="$(sed -n 's|.*;||p' <<< "$LINE")"
 	ID="$(sed 's/[ \\/]//g' <<< "$FRONT")"
 	BACKFORMATTED="$(perl -pe 's|##(.*?)#(.*?)##|<a href="#\2">\1</a>|g' <<< "$BACK")"
-	echo -e "<dt id='""$ID""'>""$FRONT"" <a href='#top'>↑</a></dt>\n<dd>"$BACKFORMATTED"</dd>" >> index.html 
+	echo -e "<dt id='""$ID""'><a href='#""$ID""' style='text-decoration:none;color:black !important;'>""$FRONT"" </a><a href='#top' style='text-decoration:none;'>↑</a></dt>\n<dd>"$BACKFORMATTED"</dd>" >> index.html
 done
 
 # wrap data in html tags and insert
